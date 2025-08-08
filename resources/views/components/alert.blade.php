c981e61bdf534cb7b1715dd6c7e2c1abbdecc8ee
Marc Sanchez <mks4nch3z@gmail.com>
2025-08-08 06:42:49 +0200

    creating component's attributes

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -23,6 +23,3 @@
     }
 @endphp
 
-<div class="p-4 mb-4 text-sm rounded-lg {{ $class }}" role="alert">
-    <span class="font-medium">{{ $titulo ?? 'Info Alert!' }}</span> {{ $slot }}
-</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Fitbit - HomePage</title>
-</head>
-<body>
+<x-layout>
     <div class="max-w-3xl mx-auto p-4">
-        <h1>Welcome to Fitbit!</h1>
-
-        <x-alert type="info">
+        <x-alert2 type="success" class="mb-4">
             <x-slot name="titulo">
                 Título Alerta
             </x-slot>
             Contenido de la alerta
-        </x-alert>
+        </x-alert2>
 
+        <p>Welcome to Fitbit</p>
     </div>
-</body>
-</html>
-
+</x-layout>
 <!-- Plantilla básica con CDN TailwindCSS. Urgente diseñar vistas del CRUD y el Home Page-->
